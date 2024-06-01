19a974faf5932431085a074e783d960fda176087
Artur Maslobojev <arthurmasl@gmail.com>
2024-06-02 00:16:18 +0300

    examples

--- src/main.go
+++ src/main.go
@@ -2,14 +2,41 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"slices"
 )
 
-func main() {
-	fmt.Println("hello22, world")
+type Vector struct {
+	x, y, z int
+}
 
-	nums := []int{1, 2, 3, 4, 5}
+func (v *Vector) sum() int {
+	return v.x + v.y + v.z
+}
 
-	for n, v := range nums {
-		fmt.Println(n, v)
+func check(e error) {
+	if e != nil {
+		panic(e)
 	}
+}
+
+func main() {
+	v1 := Vector{10, 20, 1}
+	v1.z = 55
+
+	fmt.Println(v1)
+	fmt.Println(v1.sum())
+
+	text := []byte("hello, world")
+	e := os.WriteFile("test.txt", text, 0)
+	check(e)
+
+	strings := []string{"c", "a", "b"}
+	nums := []int{5, 54, 1, 13, 65}
+
+	slices.Sort(strings)
+	slices.Sort(nums)
+
+	fmt.Println(strings)
+	fmt.Println(nums)
 }
