360404ad1d81fde0436d0d6c297a8549f9507c88
Artur Maslobojev <arthurmasl@gmail.com>
2024-06-15 21:48:11 +0300

    clean up

--- src/main.go
+++ src/main.go
@@ -1,32 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"os"
-	"os/exec"
-	"time"
-)
-
 func main() {
-	rr := 85
-	ticker := time.NewTicker(time.Second)
-	done := make(chan bool)
-
-	go func() {
-		for {
-			select {
-			case <-done:
-				return
-			case <-ticker.C:
-				cmd := exec.Command("clear")
-				cmd.Stdout = os.Stdout
-				cmd.Run()
-				rr += 5
-				fmt.Printf("%v\n", rr)
-			}
-		}
-	}()
-
-	<-done
-	fmt.Println("stop")
 }
