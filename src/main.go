edd772c76c59cb44944431e52f311ed7bc8f9462
Artur Maslobojev <arthurmasl@gmail.com>
2024-06-17 23:20:28 +0300

    clean up

--- src/main.go
+++ src/main.go
@@ -1,22 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"net/url"
-)
-
 func main() {
-	urlString := "postgres://user:pass@host.com:5432/path?k=v#f"
-	parsed, err := url.Parse(urlString)
-	if err != nil {
-		panic("oh no")
-	}
-
-	fmt.Println(parsed.Scheme)
-	fmt.Println(parsed.Opaque)
-	fmt.Println(parsed.User)
-	fmt.Println(parsed.Port())
-	fmt.Println(parsed.Path)
-	fmt.Println(parsed.RawQuery)
-	fmt.Println(parsed.Fragment)
 }
