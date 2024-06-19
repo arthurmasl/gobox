b745237e410ff3685bd0eba3933e4b8513769e3c
Artur Maslobojev <arthurmasl@gmail.com>
2024-06-19 19:45:14 +0300

    clean up

---
+++ draft/rand.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+	"math/rand/v2"
+)
+
+func main() {
+}
--- src/main.go
+++ src/main.go
@@ -1,11 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"math/rand/v2"
-)
-
 func main() {
-	fmt.Println(rand.IntN(100))
-	fmt.Println(rand.NewPCG(5, 10).Uint64())
 }
