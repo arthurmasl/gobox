86d017e06bdd24e5b1a81eb48146e42546bf84b1
Artur Maslobojev <arthurmasl@gmail.com>
2024-09-05 19:46:46 +0300

    interface

---
+++ draft/interface.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type geometry interface {
+	area() float64
+	perim() float64
+}
+
+type rect struct {
+	width, height float64
+}
+
+type circle struct {
+	radius float64
+}
+
+func (r rect) area() float64 {
+	return r.width * r.height
+}
+
+func (r rect) perim() float64 {
+	return 2*r.width + 2*r.height
+}
+
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func (c circle) perim() float64 {
+	return 2 * math.Pi * c.radius
+}
+
+func measure(g geometry) {
+	fmt.Printf("geometry: %#v, area: %v, perim: %v\n", g, g.area(), g.perim())
+}
+
+func main() {
+	r := rect{3, 4}
+	o := circle{5}
+
+	measure(r)
+	measure(o)
+}
--- src/main.go
+++ src/main.go
@@ -1,21 +1,4 @@
 package main
 
-import "fmt"
-
-func sumSlice[T int | float32 | float64](slice []T) T {
-	var sum T
-
-	for _, v := range slice {
-		sum += v
-	}
-
-	return sum
-}
-
 func main() {
-	intSlice := []int{1, 2, 3}
-	float32Slice := []float32{1, 2, 3.5}
-
-	fmt.Println(sumSlice(intSlice))
-	fmt.Println(sumSlice(float32Slice))
 }
