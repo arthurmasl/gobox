2d084c2fc226a0be6edd2c37afb1a2d1a8aaf37f
Artur Maslobojev <arthurmasl@gmail.com>
2024-09-08 23:54:13 +0300

    aoc

--- src/main.go
+++ src/main.go
@@ -1,4 +1,66 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type PlantMap struct {
+	destination int
+	source      int
+	length      int
+}
+
+var (
+	seeds  []int
+	groups [][]PlantMap
+)
+
 func main() {
+	dat, _ := os.ReadFile("src/input.txt")
+	text := string(dat)
+
+	scanner := bufio.NewScanner(strings.NewReader(text))
+	index := 0
+	groupIndex := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		// collect seeds
+		if index == 0 {
+			chunks := strings.Split(line, " ")[1:]
+			for _, chunk := range chunks {
+				seed, _ := strconv.Atoi(chunk)
+				seeds = append(seeds, seed)
+			}
+		}
+
+		// create group
+		if strings.Contains(line, "map:") {
+			groupIndex++
+			groups = append(groups, []PlantMap{})
+			continue
+		}
+
+		// fill group
+		if groupIndex > 0 && len(line) > 0 {
+			nums := strings.Split(line, " ")
+
+			destination, _ := strconv.Atoi(nums[0])
+			source, _ := strconv.Atoi(nums[1])
+			length, _ := strconv.Atoi(nums[2])
+
+			plantMap := PlantMap{destination, source, length}
+
+			groups[groupIndex-1] = append(groups[groupIndex-1], plantMap)
+		}
+
+		index++
+	}
+
+	fmt.Printf("%v\n", groups)
 }
