228f75e8337c1a742b262d21c8bde8fb0126d8f0
Artur Maslobojev <arthurmasl@gmail.com>
2024-11-21 20:09:52 +0200

    gitignore

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,11 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Println("aa")
+	file, _ := os.Open("cmd/app/testdata.txt")
+	defer file.Close()
+
+	execute(file)
 }
 
-func Sum(a, b int) int {
-	return a + b
+func execute(file *os.File) string {
+	scanner := bufio.NewScanner(file)
+	data := make(map[string][]float32)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Split(line, ";")
+		name := parts[0]
+		floatTemp, _ := strconv.ParseFloat(parts[1], 32)
+		temp := float32(floatTemp)
+
+		_, ok := data[name]
+		if !ok {
+			data[name] = make([]float32, 3)
+		}
+
+		// min
+		if data[name][0] == 0 || data[name][0] > temp {
+			data[name][0] = temp
+		}
+		// max
+		if data[name][2] < temp {
+			data[name][2] = temp
+		}
+		// mean
+		data[name][1] = (data[name][0] + data[name][2]) / 2
+	}
+
+	strs := make([]string, len(data))
+	index := 0
+	for key, value := range data {
+		strs[index] = fmt.Sprintf("%v=%v/%v/%v", key, value[0], value[1], value[2])
+		index++
+	}
+	slices.Sort(strs)
+	result := fmt.Sprintf("{%v}", strings.Join(strs, ", "))
+
+	return result
 }
--- cmd/app/main_test.go
+++ cmd/app/main_test.go
@@ -1,9 +1,27 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
-func BenchmarkSum(b *testing.B) {
+var (
+	input, _  = os.Open("./testdata.txt")
+	output, _ = os.ReadFile("./testdata.out")
+)
+
+func Test1BRC(t *testing.T) {
+	t.Run("test", func(t *testing.T) {
+		result := execute(input)
+
+		if result != string(output)[:len(output)-1] {
+			t.Fatalf("test failed")
+		}
+	})
+}
+
+func Benchmark1BRC(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Sum(2, 2)
+		execute(input)
 	}
 }
