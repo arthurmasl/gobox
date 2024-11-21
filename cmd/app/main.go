6e43d08630ff6fcc4777b527022d54a995f28e76
Artur Maslobojev <arthurmasl@gmail.com>
2024-11-21 20:56:16 +0200

    testdata2

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -7,49 +7,60 @@ import (
 	"slices"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
-	file, _ := os.Open("cmd/app/measurements.txt")
+	file, _ := os.Open("cmd/app/testdata2.txt")
+	// file, _ := os.Open("cmd/app/measurements.txt")
 	defer file.Close()
 
+	t1 := time.Now()
 	execute(file)
+	fmt.Println(time.Since(t1))
+}
+
+type Data struct {
+	min, max, sum float64
+	count         int64
 }
 
 func execute(file *os.File) string {
 	scanner := bufio.NewScanner(file)
-	data := make(map[string][]float32)
+	data := make(map[string]*Data)
 
 	for scanner.Scan() {
 		line := scanner.Text()
 		parts := strings.Split(line, ";")
 		name := parts[0]
-		floatTemp, _ := strconv.ParseFloat(parts[1], 32)
-		temp := float32(floatTemp)
+		temp, _ := strconv.ParseFloat(parts[1], 64)
 
-		_, ok := data[name]
-		if !ok {
-			data[name] = make([]float32, 3)
+		d := data[name]
+		if d == nil {
+			data[name] = &Data{
+				min:   temp,
+				max:   temp,
+				sum:   temp,
+				count: 1,
+			}
+		} else {
+			d.min = min(d.min, temp)
+			d.max = max(d.min, temp)
+			d.sum += temp
+			d.count++
 		}
 
-		// min
-		if data[name][0] == 0 || data[name][0] > temp {
-			data[name][0] = temp
-		}
-		// max
-		if data[name][2] < temp {
-			data[name][2] = temp
-		}
-		// mean
-		data[name][1] = (data[name][0] + data[name][2]) / 2
 	}
 
-	strs := make([]string, len(data))
 	index := 0
+	strs := make([]string, len(data))
+
 	for key, value := range data {
-		strs[index] = fmt.Sprintf("%v=%v/%v/%v", key, value[0], value[1], value[2])
+		mean := value.sum / float64(value.count)
+		strs[index] = fmt.Sprintf("%v=%v/%v/%v", key, value.min, mean, value.max)
 		index++
 	}
+
 	slices.Sort(strs)
 	result := fmt.Sprintf("{%v}", strings.Join(strs, ", "))
 
