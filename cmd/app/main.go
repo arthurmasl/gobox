edff3fcf6c3a8723165aaefc7d0a5fcb8035da2e
Artur Maslobojev <arthurmasl@gmail.com>
2024-11-25 01:41:47 +0200

    ignore

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,26 +1,7 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	s1 := []int{1, 9, 7, 3, 2, 4}
-
-	fmt.Println(len(s1))
-	res := bubbleSearch(s1)
-
-	fmt.Println(res)
-}
-
-func bubbleSearch(arr []int) []int {
-	for i := range arr {
-		for j := range len(arr) - i - 1 {
-			if arr[j] > arr[j+1] {
-				arr[j], arr[j+1] = arr[j+1], arr[j]
-			}
-		}
-	}
-
-	return arr
+	sum := 1200
+	sum += 99
+	println(sum)
 }
