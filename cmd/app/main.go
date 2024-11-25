25fa9bdfa33b55b6f8ce9f3a1d2c926a1e259476
Artur Maslobojev <arthurmasl@gmail.com>
2024-11-25 23:37:21 +0200

    clean up

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,26 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
-func debounce(f func(), delay time.Duration) func() {
-	var timer *time.Timer
-
-	return func() {
-		if timer != nil {
-			timer.Stop()
-		}
-		timer = time.AfterFunc(delay, f)
-	}
-}
-
 func main() {
-	debouncedFunc := debounce(func() {
-		fmt.Println("Debounced call")
-	}, 1000*time.Millisecond)
-
-	debouncedFunc()
-	time.Sleep(time.Second * 2)
 }
