61263a0c07d50ed908e2da7471a3cd45490d98a1
Artur Maslobojev <arthurmasl@gmail.com>
2024-12-03 20:39:07 +0200

    utils

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,9 +1,21 @@
 package utils
 
+import "iter"
+
 func UNUSED(x ...any) {}
 
 func Assert(condition bool) {
 	if !condition {
 		panic("Assertion failed")
+	}
+}
+
+func Window[Slice ~[]E, E any](slice Slice, size int) iter.Seq[Slice] {
+	return func(yield func(Slice) bool) {
+		for i := range slice[:len(slice)-size+1] {
+			if !yield(slice[i : i+size]) {
+				return
+			}
+		}
 	}
 }
