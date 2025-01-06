555d2a1309c3ed1a79b1fcd3a045e5ddddc7f646
Artur Maslobojev <arthurmasl@gmail.com>
2025-01-07 00:46:48 +0200

    ascii rendering

--- Makefile
+++ Makefile
@@ -9,3 +9,12 @@ test:
 
 profile:
 	@go tool pprof -http 127.0.0.1:8080 cpu_profile.prof
+
+momo:
+	@rm ./resources/momo.gif
+	@ffmpeg -i resources/momo.mov -vf "fps=30,scale=175:-1" -c:v gif resources/momo.gif
+	@go run ./cmd/app/main.go
+
+# @ffmpeg -i resources/full.mov -vf "fps=30,scale=175:-1" -c:v gif resources/full.gif
+dandadan:
+	@go run ./cmd/app/main.go
--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,4 +1,58 @@
 package main
 
+import (
+	"fmt"
+	"image/color"
+	"image/gif"
+	"os"
+	"time"
+)
+
+// const chars = "@%#*+=-:. "
+// const chars = "   :danD"
+const chars = " .,:ilwW"
+
 func main() {
+	file, err := os.Open("resources/full.gif")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	g, err := gif.DecodeAll(file)
+	if err != nil {
+		panic(err)
+	}
+
+	frames := make([]string, 0)
+	for _, i := range g.Image {
+		frame := ""
+
+		for y := i.Rect.Min.Y; y < i.Rect.Max.Y; y++ {
+			for x := i.Rect.Min.X; x < i.Rect.Max.X; x++ {
+				color := i.At(x, y)
+				char := pixelToASCII(color)
+				frame += char
+			}
+
+			frame += "\n"
+		}
+
+		frames = append(frames, frame)
+	}
+
+	for {
+		for _, frame := range frames {
+			fmt.Println(frame)
+			time.Sleep(50 * time.Millisecond)
+		}
+	}
+}
+
+func pixelToASCII(pixel color.Color) string {
+	r, g, b, _ := pixel.RGBA()
+	gray := uint8(0.299*float64(r/256) + 0.587*float64(g/256) + 0.114*float64(b/256))
+	scale := float64(gray) / 255.0
+	index := int(scale * float64(len(chars)-1))
+	return string(chars[index])
 }
