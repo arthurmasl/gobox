412c531b6bacab68f50d171baaac500ff7f9e974
Artur Maslobojev <arthurmasl@gmail.com>
2025-01-08 23:30:34 +0200

    ref

--- examples/todo/utils.go
+++ examples/todo/utils.go
@@ -1,14 +1,5 @@
 package main
 
-import "fmt"
-
-func clearConsole() {
-	// cmd := exec.Command("clear")
-	// cmd.Stdout = os.Stdout
-	// cmd.Run()
-	fmt.Print("\033[H\033[2J")
-}
-
 func getPrefix(item *Item) string {
 	prefix := separator
 	if item.focused {
