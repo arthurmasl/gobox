d28694dd8393a3ad50452a582b13584eecba13bc
Artur Maslobojev <arthurmasl@gmail.com>
2025-01-12 23:22:24 +0200

    interface

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,31 +4,31 @@ import (
 	"fmt"
 )
 
-type player struct {
-	pos complex128
-	hp  int
-}
-
-type enemy struct {
-	pos   complex128
-	hp    int
-	aggro bool
-}
-
-func (this *player) draw()   { fmt.Printf("player drawed at %v\n", this.pos) }
-func (this *player) update() { this.pos += 1 }
-
-func (this *enemy) draw()   { fmt.Printf("enemy drawed at %v\n", this.pos) }
-func (this *enemy) update() { this.pos += 2 }
-
 type entity interface {
 	update()
 	draw()
 }
 
+type player struct {
+	pos complex128
+	hp  int
+}
+
+func (this *player) draw()   { fmt.Printf("drawed at (%v:%v)\n", real(this.pos), imag(this.pos)) }
+func (this *player) update() { this.pos += 1 }
+
+type creature struct {
+	pos   complex128
+	hp    int
+	count int
+}
+
+func (this *creature) draw()   { fmt.Printf("drawed at (%v:%v)\n", real(this.pos), imag(this.pos)) }
+func (this *creature) update() { this.pos += 2 }
+
 func main() {
 	entities := make([]entity, 0)
-	entities = append(entities, &player{}, &enemy{})
+	entities = append(entities, &player{hp: 100}, &creature{hp: 50, count: 10})
 
 	for _, entity := range entities {
 		fmt.Printf("%T\n", entity)
@@ -37,11 +37,9 @@ func main() {
 
 		switch entity := entity.(type) {
 		case *player:
-			fmt.Println(entity.hp, entity.pos)
-			fmt.Println("player")
-		case *enemy:
-			fmt.Println(entity.aggro)
-			fmt.Println("enemy")
+			fmt.Printf("player %vhp\n", entity.hp)
+		case *creature:
+			fmt.Printf("enemy %vhp\n", entity.hp)
 		}
 
 		fmt.Println()
